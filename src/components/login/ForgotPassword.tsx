dd89723156bf991f6d6adf6b89bed3fad75f94d9
Julian <julian@vinneren.com.mx>
2024-07-04 14:21:48 -0600

    quitar efectos

--- src/components/login/ForgotPassword.tsx
+++ src/components/login/ForgotPassword.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { Link } from "react-router-dom";
 import * as Yup from "yup";
